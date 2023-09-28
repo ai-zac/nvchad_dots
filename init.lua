affb0cbdc1f713f406c56d5c4d6dfe9c177a1219
ai-zac <isaacjdv31@gmail.com>
2023-09-27 21:15:57 -0600

    update init

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
 vim.opt.breakindent = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
@@ -20,10 +13,3 @@ vim.notify = function(msg, ...)
 
     notify(msg, ...)
 end
--- vim.cmd [[ colorscheme themer_gruvbox]]
--- vim.cmd([[
---   augroup COLOR
---     autocmd!
---     autocmd ColorScheme	* colorscheme themer_gruvbox
---   augroup END
--- ]])
